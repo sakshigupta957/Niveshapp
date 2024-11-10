6010cef07cf6a3883936f33d519e3119dc775108
Sakshi Gupta <sakshigupta957111@gmail.com>
2024-11-10 15:50:07 +0530

    Create extra.js

---
+++ frontend/src/groups/pages/extra.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../context/auth_context';
+import "./extra.css";
+const Extra = props => {
+    const auth = useContext(AuthContext);
+  
+    return (<div>
+    
+      
+      </div>
+    );
+  };
+  
+
+export default Extra;
