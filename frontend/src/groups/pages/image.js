dca2350d7565221a02023ea6f7374e010b54f80b
Sakshi Gupta <sakshigupta957111@gmail.com>
2024-11-10 15:55:00 +0530

    Create image.js

---
+++ frontend/src/groups/pages/image.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../context/auth_context';
+import "./image.css";
+import nivesh from '../../images/NIVESH.png';
+
+const Image = props => {
+    const auth = useContext(AuthContext);
+  
+    return (
+      
+
+<div className="image_div">
+	<figure>
+    <img src={nivesh}></img>
+  </figure>
+  <div class="inner"></div>
+			<div class="inner inner2"></div>
+</div>
+      
+      
+    );
+  };
+  
+
+export default Image;
